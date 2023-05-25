f581c08e210e66c25b3a186d3f8139569edab33b
webcodes <dev@wbcod.com>
2023-05-26 00:33:59 +0200

    update packages

--- config/app.php
+++ config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Collective\Html\HtmlServiceProvider::class,
+        Spatie\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
